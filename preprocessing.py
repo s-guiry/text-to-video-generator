0c2d7931972ac4c531fd770627b4e6895c923d04
davidhabboosh1 <habboosh.d@northeastern.edu>
2024-04-04 13:43:43 -0400

    things

--- preprocessing.py
+++ preprocessing.py
@@ -1,63 +1,3 @@
-# import numpy as np
-# import os
-# import cv2
-
-# # replace this with the actual data path
-# #path = 'datasets/kinetics-dataset-main/k700-2020/'
-# path = 'test-samples/'
-# fps = 10
-
-# # get the list of files in the train, test, and val folders
-# train_files = os.listdir(path + 'train')
-# test_files = os.listdir(path + 'test')
-# val_files = os.listdir(path + 'val')
-
-# # create a np array to store the videos with their labels -- each folder is the label of all the videos in it
-# dataset = []
-
-# # read the train videos
-# def read_videos(split):
-#     dir = path + split + '/' # path as text
-    
-#     for folder in os.listdir(dir):
-#         folder_path = dir + folder + '/'
-        
-#         for file in os.listdir(folder_path):
-#             cap = cv2.VideoCapture(folder_path + file)
-#             cap.set(cv2.CAP_PROP_FPS, fps)
-#             frames = []
-#             for _ in range(fps * 5):
-#                 ret, frame = cap.read()
-#                 if not ret:
-#                     break
-#                 frames.append(cv2.resize(frame, (128, 128)))
-#             dataset.append(np.array([np.array(frames), folder]))
-#             cap.release()
-    
-#     f = open(split + ".txt", "w")
-#     f.write(split + " is done")
-#     f.close()
-            
-# read_videos('train')
-# read_videos('test')
-# read_videos('val')
-
-# # convert the dataset to a np array
-# dataset = np.array(dataset)
-# np.save('dataset.npy', dataset)
-
-# # load dataset.npy
-# ds = np.load('dataset.npy', allow_pickle=True)
-
-# print(ds.shape)
-
-# print()
-
-# # also print shape of each video
-# for i in range(10):
-#     print(ds[i][0].shape, ds[i][1])
-
-
 import numpy as np
 import os
 import cv2
@@ -102,7 +42,11 @@ def process_video(video_path, gpu):
 def process_videos_parallel(video_paths):
     with concurrent.futures.ThreadPoolExecutor() as executor:
         resized_videos = list(executor.map(process_video, video_paths, [USE_GPU] * len(video_paths)))
-    print('FINISHED SPLIT')
+    
+    # create a text file to indicate that the processing is done
+    f = open(video_paths[0].split('/')[-3] + ".txt", "w")
+    f.write(video_paths[0].split('/')[-3] + " is done")
+    
     return resized_videos
 
 # Paths
