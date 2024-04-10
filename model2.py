a850095aed339e4aa89d314bbf7a891e691d5bb4
davidhabboosh1 <habboosh.d@northeastern.edu>
2024-04-10 11:25:22 -0400

    a

--- model2.py
+++ model2.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from keras.layers import Input, Conv3D, concatenate, UpSampling3D
 from keras.layers import Reshape, Permute
 import threading
+from tqdm import tqdm
 
 VIDEO_SHAPE = (224, 224)
 
@@ -16,31 +17,24 @@ def noise_predictor(input_size, label_size, timestep_size):
     label_inputs = Input(label_size)
     timestep_inputs = Input(timestep_size)
     
-    # Reshape and permute label_inputs to match the spatial dimensions of inputs
     label_reshaped = Reshape((1, 1, label_size[0], 1))(label_inputs)
     label_permuted = Permute((4, 1, 2, 3))(label_reshaped)
     label_tiled = tf.tile(label_permuted, [1, 50, input_size[1], input_size[2], 1])
     
-    # Reshape and permute timestep_inputs to match the spatial dimensions of inputs
     timestep_reshaped = Reshape((1, 1, timestep_size[0], 1))(timestep_inputs)
     timestep_permuted = Permute((4, 1, 2, 3))(timestep_reshaped)
     timestep_tiled = tf.tile(timestep_permuted, [1, 50, input_size[1], input_size[2], 1])
     
-    # Concatenate input tensors along the last axis
     concatenated_input = concatenate([inputs, label_tiled, timestep_tiled], axis=-1)
 
-    # Define convolutional layers
     conv1 = Conv3D(64, 3, activation='relu', padding='same')(concatenated_input)
     conv2 = Conv3D(128, 3, activation='relu', padding='same')(conv1)
     
-    # Upsampling and additional convolutional layer
     up1 = UpSampling3D(size=(1, 1, 1))(conv2)
     conv3 = Conv3D(64, 3, activation='relu', padding='same')(up1)
     
-    # Output layer
-    output = Conv3D(3, 1, activation='sigmoid')(conv3)  # Assuming RGB channels
+    output = Conv3D(3, 1, activation='sigmoid')(conv3)
     
-    # Define and return the model
     model = tf.keras.Model(inputs=[inputs, label_inputs, timestep_inputs], outputs=output)
     return model
 
@@ -54,16 +48,14 @@ def reintegrate_noise(denoised_video, predicted_noise, reintegration_factor):
 
 def iterative_diffusion_loss(true_noise, noisy_video, timestep, label_data, noise_predictor_model, reintegration_factor, num_iterations):
     total_loss = 0.0
-    denoised_video = tf.expand_dims(noisy_video, axis=0)  # Initialize denoised video with noisy video
-    predicted_noise = tf.zeros_like(denoised_video[0])  # Initial noise prediction
+    denoised_video = tf.expand_dims(noisy_video, axis=0)
+    predicted_noise = tf.zeros_like(denoised_video[0])
     label_data = tf.expand_dims(tf.convert_to_tensor(label_data), axis=0)
     timestep = tf.expand_dims(tf.convert_to_tensor(timestep), axis=0)
 
     for _ in range(num_iterations):
         predicted_noise_label = noise_predictor_model([denoised_video, label_data, timestep])
         predicted_noise = noise_predictor_model([denoised_video, np.zeros_like(label_data), timestep])
-        
-        # take the difference between the predictions and amplify the noise
         predicted_noise = np.abs((predicted_noise - predicted_noise_label) * 2.0)
         
         iteration_loss = tf.keras.losses.mean_squared_error(predicted_noise, true_noise)
@@ -73,72 +65,49 @@ def iterative_diffusion_loss(true_noise, noisy_video, timestep, label_data, nois
 
     return total_loss
 
-# Define model and optimizer
-input_shape = (50, *VIDEO_SHAPE, 3)  # Assuming RGB channels
-label_shape = (512,)  # Assuming the label shape
-timestep_shape = (1,)  # Assuming the timestep shape
+input_shape = (50, *VIDEO_SHAPE, 3)
+label_shape = (512,)
+timestep_shape = (1,)
 noise_predictor_model = noise_predictor(input_shape, label_shape, timestep_shape)
 optimizer = tf.keras.optimizers.Adam()
 
-# Train the model
 num_epochs = 100
 reintegration_factor = 0.9
 
-# Define function for threaded dataset loading
 def load_dataset(dataset, idx):
     dataset[idx] = np.load(f'dataset_p{idx}.npy', allow_pickle=True)
 
 for epoch in range(num_epochs):
     print(f'On epoch {epoch}', flush=True)
     
-    # Initialize dataset list for storing loaded datasets
     datasets = [None] * 100
-    
-    # Initialize threads for loading datasets
     threads = [threading.Thread(target=load_dataset, args=(datasets, i)) for i in range(100)]
     
-    # Start and join all threads for loading datasets
     for t in threads:
         t.start()
     for t in threads:
         t.join()
     
-    # Training loop
-    for dataset in datasets:
-        if dataset is None:
-            print('dataset is none', flush=True)
-            continue
-        
-        for batch in dataset:
-            video_data = batch[0]
-            label_data = batch[1]
+    with tqdm(datasets, desc=f'Epoch {epoch}', unit='dataset') as pbar:
+        for dataset in pbar:
+            if dataset is None:
+                continue
             
-            print('got data', flush=True)
-            
-            T = np.random.randint(1, 101)
-            true_noise = get_noise(video_data.shape, timestep=T)  # Generate true noise
-            
-            print('generated noise', flush=True)
-            
-            with tf.GradientTape() as tape:
-                loss = iterative_diffusion_loss(true_noise, video_data + true_noise, T, label_data, noise_predictor_model, reintegration_factor, num_iterations=100)
-            
-            print('calculated loss', flush=True)
-            
-            gradients = tape.gradient(loss, noise_predictor_model.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, noise_predictor_model.trainable_variables))
-            
-            print('applied gradients', flush=True)
-            
-            if np.random.randint(10) == 5:
-                print('Still going!', flush=True)
+            for batch in dataset:
+                video_data = batch[0]
+                label_data = batch[1]
                 
-            break # temp
-        
-        break # temp
-                        
-    print(f'Epoch {epoch} loss: {loss}\n', flush=True)
-        
-# Save the trained model
-noise_predictor_model.save('noise_predictor_model.h5')
-print('Done training!', flush=True)
+                T = np.random.randint(1, 101)
+                true_noise = get_noise(video_data.shape, timestep=T)
+                
+                with tf.GradientTape() as tape:
+                    loss = iterative_diffusion_loss(true_noise, video_data + true_noise, T, label_data, noise_predictor_model, reintegration_factor, num_iterations=100)
+                
+                gradients = tape.gradient(loss, noise_predictor_model.trainable_variables)
+                optimizer.apply_gradients(zip(gradients, noise_predictor_model.trainable_variables))
+                
+                # Update progress bar description
+                pbar.set_description(f'Epoch {epoch} - Loss: {loss.numpy():.4f}')
+                
+                if np.random.randint(10) == 5:
+                    pbar.write('Still going!')
