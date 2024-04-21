1ad812c6a3febcf3c2a89b87cd67271af5cf5fe9
attanyl <70954240+attanyl@users.noreply.github.com>
2024-04-21 17:06:58 -0400

    Dynamic adjustment

--- usemodel.py
+++ usemodel.py
@@ -25,14 +25,19 @@ with tqdm(total=T + 1, desc='Generating Noise') as pbar:
     for i in range(T + 1):
         noise = model.predict([initial_noise, replicated_embedding, replicated_timestep])
         normalized_noise = (noise - np.min(noise)) / (np.max(noise) - np.min(noise)) * 2 - 1
-        new_noise = 0.5 * normalized_noise
-        initial_noise = 0.75 * initial_noise + new_noise
-        initial_noise = (initial_noise - np.min(initial_noise)) / (np.max(initial_noise) - np.min(initial_noise)) * 2 - 1
+
+        std_dev = np.std(initial_noise)
+        update_weight = max(0.1, 1 - std_dev)
+        initial_noise += update_weight * normalized_noise
+        noise_random_factor = np.random.normal(0, 0.1, initial_noise.shape)
+        initial_noise += noise_random_factor
+
+        if i % 20 == 0:
+            initial_noise = get_noise((1, 50, 224, 224, 3), T)
 
         frame = ((initial_noise[0, 0] + 1) * 127.5).astype(np.uint8)
         frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         video.write(frame_bgr)
-        
         pbar.update(1)
 
 video.release()
